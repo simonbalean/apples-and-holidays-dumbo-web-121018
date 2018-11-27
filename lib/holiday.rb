f5ba33e4d80e5360dec861b78396ccc0e1229409
Simon Balean <simonbalean@gmail.com>
2018-11-27 17:48:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supply|
-      puts "  #{holiday.to_s.gsub!("_", " ").titalize}: #{supply.join(", ")}"
+      puts "  #{holiday.to_s.gsub!("_", " ").titelize}: #{supply.join(", ")}"
     end
   end
 end
